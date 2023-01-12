3f7c9c053bfe1b0160e860a1ecc1cda8668ff101
zych.johann@gmail.com <zych.johann@gmail.com>
2023-01-12 16:02:10 +0100

    Home Page 2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,8 +14,12 @@ class HomeController extends AbstractController
     public function index(VehicleRepository $vehicleRepository): Response
     {
         $cars = $vehicleRepository->findAll();
+        $carSport = $vehicleRepository->findBy(['type' => 'sportive']);
+        $carUtility = $vehicleRepository->findBy(['type' => 'utilitaire']);
         return $this->render('home/index.html.twig', [
             'cars' => $cars,
+            'carSport' => $carSport,
+            'carUtility' => $carUtility,
             'controller_name' => 'HomeController',
         ]);
     }
